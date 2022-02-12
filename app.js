45d3304a8296380e5b71dfd5cd20e348bb9ddb78
jamesaudry <james.audry@gmail.com>
2022-02-12 10:47:23 -0500

    made changes

--- app.js
+++ app.js
@@ -5,25 +5,22 @@ const app = express();
 const morgan = require('morgan')
 const postBank = require('./postBank')
 const publicDir = express.static('public')
+// app.use(express.static('public'));
 
 
 
 
-let posts = postBank.list()
 app.use(publicDir)
 
 
 
 app.use(morgan('dev'))
 
-app.get('/', (req, res, next) => {
-  let mappedBank = postBank.list().map(cur => {
-    return [cur.title, cur.name]
-  })
+app.get('/', (req, res, next) => { 
+  let posts = postBank.list()
+
   
-  
-  
-  const otherHTML = `<!DOCTYPE html>
+  const html = `<!DOCTYPE html>
   <html>
   <head>
     <title>Wizard News</title>
@@ -35,7 +32,8 @@ app.get('/', (req, res, next) => {
       ${posts.map(post => `
         <div class='news-item'>
           <p>
-            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
             <small>(by ${post.name})</small>
           </p>
           <small class="news-info">
@@ -46,59 +44,68 @@ app.get('/', (req, res, next) => {
     </div>
   </body>
 </html>`
+res.send(html);
+
+});
     
 
 
-
-
-const html = `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-  	<meta charset="UTF-8">
-  	<title>Document</title>
-  </head>
-  <body>
-  	<ul>
-  		${postBank.list().map(post=>{
-  		return `<li>${post.title} : ${post.name}</li>`
-  	}).join('')}
-  	</ul>
-  </body>
-  </html>`
-  res.send(otherHTML)
-  next()
-})
-
-
 app.get('/posts/:id', (req, res) => {
-  const id = req.params.id
- 
+  const id = req.params.id;
+  const post = postBank.find(id);
+if (!post.id) {
+  // throw new Error('Not Found')
   
-  const post = postBank.find(id)
-  
-  
-  const otherHTML = `<!DOCTYPE html>
+  // If the post wasn't found, set the HTTP status to 404 and send Not Found HTML
+  res.status(404)
+  const html = `
+  <!DOCTYPE html>
   <html>
   <head>
     <title>Wizard News</title>
     <link rel="stylesheet" href="/style.css" />
   </head>
   <body>
-    <div class="news-list">
-      <header><img src="/logo.png"/>Wizard News</header>
-     ${post.content}
+    <header><img src="/logo.png"/>Wizard News</header>
+    <div class="not-found">
+      <p>Accio Page! 🧙‍♀️ ... Page Not Found</p>
+      <iframe src="https://giphy.com/embed/p3RblYx4T7vmU" width="480" height="378" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+      <p><a href="https://giphy.com/gifs/harry-potter-dumbledore-p3RblYx4T7vmU">via GIPHY</a></p>
     </div>
   </body>
-</html>`
-  res.send(otherHTML)
+  </html>`
+  res.send(html)
+} else {
+    const html = `<!DOCTYPE html>
+    <html>
+    <head>
+    <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+          <div class='news-item'>
+            <p>
+              ${post.title} <small>(by ${post.name})</small>
+            </p>
+            <p>
+              ${post.content}
+            </p>
+          </div>
+      </div>
+    </body>
+    </html>`
+    res.send(html)
+}
 
-})
+});
 
 app.get('/stuff', (req, res) => {
   res.send('<img src="logo.png" alt="">')
-})
+});
 
-const { PORT = 1337 } = process.env;
+const PORT = 1337 
 
 
 
